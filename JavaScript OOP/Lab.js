c14090eea1cede35eba981190739509d97e9de7f
TAKZOBYE <takerchanel@gmail.com>
2020-05-16 14:55:24 +0700

    first commit

---
+++ JavaScript OOP/Lab.js
@@ -0,0 +1,27 @@
+class UserTemplate {
+    constructor(name, password, age) {
+        this.name = name
+        this.password = password
+        this.age = age
+        this.connected = false
+    }
+
+    login(){
+        this.connected = true 
+        console.log("your login status : ", this.connected)
+    }
+
+    logout(){
+        this.connected = false 
+        console.log("your login status : ", this.connected)
+    }
+
+    checkStatus(){
+        if (this.connected == true) {
+            console.log("You are now login.")
+        } else {
+            console.log("You are not currently login.")
+        }
+    }
+
+}
